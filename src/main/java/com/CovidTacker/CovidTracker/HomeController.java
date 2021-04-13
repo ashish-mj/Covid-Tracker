c110680885eb7579ffd7c7a3adce35866f2a66c2
Ashish M J <ashm.jagadeesh@gmail.com>
2021-04-13 13:24:55 +0530

    Fix HTML issue

--- src/main/java/com/CovidTacker/CovidTracker/HomeController.java
+++ src/main/java/com/CovidTacker/CovidTracker/HomeController.java
@@ -1,10 +1,12 @@
 package com.CovidTacker.CovidTracker;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
+	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
